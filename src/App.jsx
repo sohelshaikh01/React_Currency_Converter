7e02ab04e1627d4d52edfc956c8da2372993376a
Sohel <sohel.techin@gmail.com>
2025-01-19 20:00:47 +0530

    Error Solving of converting currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { InputBox } from './components';
+import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo.js";
 import { useState } from 'react';
 
@@ -7,9 +7,9 @@ import { useState } from 'react';
 function App() {
 
 	const [amount, setAmount] = useState(0);
+	const [convertedAmount, setConvertedAmount] = useState(0);
 	const [from, setFrom] = useState("usd");
 	const [to, setTo] = useState("inr");
-	const [convertedAmount, setConvertedAmount] = useState(0);
 
 	const currencyInfo = useCurrencyInfo(from);
 
@@ -18,10 +18,14 @@ function App() {
 	const swap = () => {
 		setFrom(to);
 		setTo(from);
-		setConvertedAmount(amount);
 		setAmount(convertedAmount);
+		setConvertedAmount(amount);
 	}
 
+	const convert = () => {
+		setConvertedAmount(amount * currencyInfo[to]);
+	}
+	
 
   return (
 	<>
@@ -31,10 +35,11 @@ function App() {
 			}}
 		>
 			<div className="w-full">
-				<div className="w-full max-w-md mx-auto border border-gray-600 rounded-lg p-5 backdrop-blur-sm bg-white/20">
+				<div className="w-full max-w-md mx-auto border border-gray-600 rounded-xl p-5 backdrop-blur-sm bg-white/20">
 					<form
 						onSubmit={(e) => {
 							e.preventDefault();
+							convert();
 						}}
 					>
 						<div className="w-full mb-1 bg-slate-50 rounded-lg">
@@ -42,8 +47,8 @@ function App() {
 								label="From"
 								amount={amount}
 								currencyOptions={options}
-								onCurrencyChange={(currency) => setFrom(currency)}
 								selectCurrency={from}
+								onCurrencyChange={(currency) => setFrom(currency)}
 								onAmountChange={(amount) => setAmount(amount)}
 							/>
 							
@@ -54,7 +59,6 @@ function App() {
 								type="button"
 								className="absolute left-1/2 -translate-x-1/2 -translate-y-1/4 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
 								onClick={swap}
-								
 							> 
 								swap
 							</button>
@@ -68,6 +72,7 @@ function App() {
 								onCurrencyChange={(currency) => setTo(currency)}
 								selectCurrency={to}
 								amountDisable
+								
 							/>
 						</div>
 
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,5 +1,6 @@
 import { useId } from "react";
 
+
 function InputBox({
     label,
     amount,
@@ -13,12 +14,10 @@ function InputBox({
 }) {
 
     const amountInputId = useId();
-   
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex container mx-auto ${className}`}>
             <div className="w-1/2">
-
                 <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                     {/* Optimising the label & Adding accessibility */}
@@ -34,9 +33,9 @@ function InputBox({
                     onChange={(e) => onAmountChange &&
                         onAmountChange(Number(e.target.value))
                         // Arguments Always taken as string in functions
-                    }
-
+                    } 
                 />
+
             </div>
 
             <div className="w-1/2 flex flex-wrap justify-end text-right">
@@ -44,12 +43,12 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
 
                 <select
-                    className="rounded-lg px-1 py-1 bg-slate-300 text-white cursor-pointer outline-none" value={selectCurrency} 
+                    className="rounded-md p-1 bg-slate-300 text-white cursor-pointer outline-none" value={selectCurrency} 
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)} disabled={currencyDisable}
                 >
                     
                     {currencyOptions?.map((currency) => (
-                        <option className="bg-slate-200 text-black" key={currency} value={currency}> {currency} </option>
+                        <option className="bg-slate-200 text-black px-2 py-1 text-center" key={currency} value={currency}> {currency} </option>
                         // Use key to track rendering that increases performance in loop
                     ))}
                 </select>
